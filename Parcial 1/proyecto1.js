4800b766ad2b36ae8a20a37261af61739b69d1e4
Ana Beltran <124410707+anazbeltran@users.noreply.github.com>
2023-09-26 19:04:33 -0700

    Add files via upload

---
+++ Parcial 1/proyecto1.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    var valor_dado1 = 0;
+    var total1 = 0;
+    var quedo1 = false;
+
+    var valor_dado2 = 0;
+    var total2 = 0;
+    var quedo2 = false;
+
+
+    $("#boton1").on("click", function () {
+        if (!quedo1) {
+            valor_dado1 = Math.floor(Math.random() * 10 + 1);
+            console.log("Jugador 1 sacó un: " + valor_dado1);
+            $("#dado1").attr("src", "imagenesproyecto/carta" + valor_dado1 + ".PNG");
+            $("#numero1").html(valor_dado1);
+
+            total1 += valor_dado1;
+            $("#total1").html("Acumuladas: " + total1);
+        }
+        if(!total1 == )
+    });
+
+    $("#mequedo1").on("click", function () {
+        quedo1 = true;
+        $("#boton1").attr("disabled", true);
+        $("#mequedo1").attr("disabled", true);
+        $("#total1").html("<h1>Total Jugador 1: " + total1 + "</h1>");
+        $("#numero1").hide();
+        Ganador();
+    });
+
+    $("#boton2").on("click", function () {
+        if (!quedo2) {
+            valor_dado2 = Math.floor(Math.random() * 10 + 1);
+            console.log("Jugador 2 sacó un: " + valor_dado2);
+            $("#dado2").attr("src", "imagenesproyecto/carta" + valor_dado2 + ".PNG");
+            $("#numero2").html(valor_dado2);
+
+            total2 += valor_dado2;
+            $("#total2").html("Acumuladas: " + total2);
+        }
+    });
+
+    $("#mequedo2").on("click", function () {
+        quedo2 = true;
+        $("#boton2").attr("disabled", true);
+        $("#mequedo2").attr("disabled", true);
+        $("#total2").html("<h1>Total Jugador 2: " + total2 + "</h1>");
+        $("#numero2").hide();
+        Ganador();
+    });
+
+    function Ganador() {
+        if (quedo1 && quedo2) {
+            var puntaje1 = Math.abs(21 - total1);
+            var puntaje2 = Math.abs(21 - total2);
+            
+            if (puntaje1 < puntaje2) {
+                alert("Jugador 1 gana.");
+            } else if (puntaje2 < puntaje1) {
+                alert("Jugador 2 gana.");
+            
+        }
+    }
+    }
+});
