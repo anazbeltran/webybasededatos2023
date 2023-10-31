f27bb24a846ae0edfe85231ce18d5ec835b440c6
Ana Beltran <124410707+anazbeltran@users.noreply.github.com>
2023-10-30 19:19:33 -0700

    Add files via upload

--- Parcial 2/menu.php
+++ Parcial 2/menu.php
@@ -1,36 +1,38 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #1F618D;">
+
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Cetis 107</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">Cetis</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="inicio.php">Inicio</a>
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Opciones</a>
+          <a class="nav-link" href="#">Alumnos</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Link
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Menu
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="inicio.php">Inicio</a></li>
-            <li><a class="dropdown-item" href="consultarAlumnos.php">Consultar alumnos</a></li>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="inicioo.php">inicio</a></li>
+            <li><a class="dropdown-item" href="consultarAlumnos.php">consultar alumnos</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Registrar alumnos</a></li>
+            <li><a class="dropdown-item" href="RegistrarAlumnos.php">Registrar Alumnos</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Link</a>
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Clave</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+      <form class="d-flex">
+        <input class="form-control me-2" type="Buscar" placeholder="Buscar" aria-label="Buscar">
         <button class="btn btn-outline-success" type="submit">Buscar</button>
       </form>
     </div>
   </div>
+
 </nav>
